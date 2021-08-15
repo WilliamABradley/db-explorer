d2b5b97d21cf20a495b0f3007ec5118476a919b9
William Bradley <williamabradley@outlook.com>
2021-08-15 15:52:04 +1200

    Initial commit

---
+++ windows/db_explorer/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::db_explorer::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::db_explorer::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
