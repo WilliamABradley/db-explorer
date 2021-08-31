61327f7ea334dec54c38aefeee1a26d0689f690d
William Bradley <williamabradley@outlook.com>
2021-09-01 10:57:22 +1200

    Openssl build attempt

--- scripts/configureNativeToolchains.js
+++ scripts/configureNativeToolchains.js
@@ -1,11 +1,28 @@
 const os = require('os');
 const { exec, platforms, isWindows } = require('./utils');
 const ndk = require('./utils/ndk');
-const { fetchOpenSSLScripts, ensureOpenSSLToolsReady } = require('./utils/openssl');
 
 const addTargets = (platform) => {
   for (const target of Object.keys(platform.targets)) {
     exec(`rustup target add ${target}`);
+  }
+};
+
+const ensurePerl = () => {
+  let perlPath;
+  switch (os.platform()) {
+    case 'win32':
+      perlPath = exec('where.exe perl', { stdio: 'pipe' });
+      break;
+
+    default:
+      perlPath = exec('which perl', { stdio: 'pipe' });
+      break;
+  }
+
+  if (!perlPath) {
+    console.error('Perl needs to be installed (Use MSYS2 on Windows)');
+    process.exit(-1);
   }
 };
 
@@ -17,9 +34,7 @@ switch (os.platform()) {
     console.log();
     ndk.prepareNDK();
 
-    ensureOpenSSLToolsReady();
-
-    fetchOpenSSLScripts();
+    ensurePerl();
     break;
 
   case 'darwin':
