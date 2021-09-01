718473c9d6f7f7f439775a9d403b76a7e4298f98
William Bradley <williamabradley@outlook.com>
2021-09-01 12:19:46 +1200

    Added safe area view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { StyleSheet, View, Text, Button, ScrollView } from 'react-native';
+import { StyleSheet, View, Text, Button, ScrollView, SafeAreaView } from 'react-native';
 import Editor from './components/Editor';
 import DatabaseDriver from './drivers/DatabaseDriver';
 import PostgresDriver from './drivers/postgres';
-import NativeMessageDatabaseDriver from './drivers/NativeMessageDatabaseDriver';
 
 let driver: DatabaseDriver;
 let driverConnect: Promise<void> | undefined;
@@ -14,36 +13,38 @@ export default function App() {
   const [results, setResults] = useState('');
 
   return (
-    <View style={styles.container}>
-      <Editor value={sql} onChange={setSQL} />
-      <Button
-        title="Run"
-        onPress={() => {
-          if (!driver) {
-            driver = new PostgresDriver({
-              host: '192.168.1.10',
-              port: '5432',
-              username: 'postgres',
-              database: 'api',
-              ssl: false,
-            });
-            driverConnect = driver
-              .connect();
-          }
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Editor value={sql} onChange={setSQL} />
+        <Button
+          title="Run"
+          onPress={() => {
+            if (!driver) {
+              driver = new PostgresDriver({
+                host: '192.168.1.10',
+                port: '5432',
+                username: 'postgres',
+                database: 'api',
+                ssl: false,
+              });
+              driverConnect = driver
+                .connect();
+            }
 
-          driverConnect!
-            .then(() => driver.query(sql))
-            .then(results => setResults(JSON.stringify(results, null, 4)))
-            .catch(e => {
-              console.error(e);
-            });
-        }}
-      />
-      <Text>Results:</Text>
-      <ScrollView>
-        <Text>{results}</Text>
-      </ScrollView>
-    </View>
+            driverConnect!
+              .then(() => driver.query(sql))
+              .then(results => setResults(JSON.stringify(results, null, 4)))
+              .catch(e => {
+                console.error(e);
+              });
+          }}
+        />
+        <Text>Results:</Text>
+        <ScrollView>
+          <Text>{results}</Text>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
