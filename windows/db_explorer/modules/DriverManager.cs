04246674552be22b7c53c6ebd1fddd38cfa6e112
William Bradley <williamabradley@outlook.com>
2021-09-01 18:06:57 +1200

    Removed c# postgres driver

--- windows/db_explorer/modules/DriverManager.cs
+++ windows/db_explorer/modules/DriverManager.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
-namespace db_explorer.modules.drivers
+namespace db_explorer.modules
 {
     [ReactModule(nameof(DriverManager))]
     public class DriverManager
