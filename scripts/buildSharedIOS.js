5d7679d7e1ab95be70527fba32f7d7f2bbd7e26e
William Bradley <williamabradley@outlook.com>
2021-09-07 13:18:24 +1200

    Added pod install to ios shared build.

--- scripts/buildSharedIOS.js
+++ scripts/buildSharedIOS.js
@@ -1,7 +1,11 @@
-const { exec, rust } = require('./utils');
+const { exec, rust, platforms } = require('./utils');
 
 console.log(`Building iOS universal`);
 
 exec(`cargo lipo --features "ios" --no-default-features --release`, {
   cwd: rust.dir,
+});
+
+exec(`pod install`, {
+  cwd: platforms.ios.dir,
 });
