db4465659292b5dd3db0089406a96b1aa925ca38
William Bradley <williamabradley@outlook.com>
2021-09-19 14:50:45 +1200

    Renamed drivers to modules, removed legacy interface.

--- ios/db_explorer/drivers/NativeDatabaseDriver.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-protocol NativeDatabaseDriver : NSObject {
-  func create(_ connectionInfo: NSDictionary, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void
-  func connect(_ id: NSInteger, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void
-  func close(_ id: NSInteger, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void
-  func execute(_ id: Int, sql: String, variables: NSDictionary?, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void
-  func flush(_ resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) -> Void
-}
